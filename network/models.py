f736a12b57c9edf64e943d9e0a875fc1fa531a34
Kevin Shitaye <it.kevin.shitaye@gmail.com>
2020-10-12 13:31:42 +0300

    created the models, tweet views and posting

--- network/models.py
+++ network/models.py
@@ -1,6 +1,25 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from datetime import datetime
 
 class User(AbstractUser):
     pass
+
+
+class Tweet(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="twitter")
+    content = models.CharField(max_length=500)
+    date = models.DateTimeField(default=datetime.now())
+    likes = models.IntegerField()
+
+    def sterilize(self):
+        return {
+            "user":self.user,
+            "content":self.content,
+            "date":self.date,
+            "likes":self.likes
+        }
+
+class Follow(models.Model):
+    follower = models.ForeignKey("User", on_delete=models.CASCADE, related_name="follower")
+    following = models.ForeignKey("User", on_delete=models.CASCADE, related_name="following")
--- network/views.py
+++ network/views.py
@@ -3,8 +3,9 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.http import JsonResponse
 
-from .models import User
+from .models import User, Tweet, Follow
 
 
 def index(request):
@@ -61,3 +62,37 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+def post(request):
+    if request.method != "POST":
+        return JsonResponse({"error": "POST request required."}, status=400)
+    
+    data = json.loads(request.body)
+
+    tweet = Tweet(
+        user = request.user
+        content = data.get("content", "")
+        likes = 0
+    )
+    tweet.save()
+
+    return JsonResponse({"sucess": "Tweet made."}, status=400)
+
+def tweets(request, page):
+    tweets = Tweet.objects.all()
+    # getting tweets accordingly  
+    if page == "profile_page":
+        tweets = Tweet.objects.filter(
+            user = request.user
+        )
+    elif page == "following_page":
+        tweets = Tweet.objects.filter(
+            user__in = Follow.objects.filter(following=request.user)
+        )
+    else:
+        return JsonResponse({"error": "Invalid mailbox."}, status=400)
+
+    # ordering the tweets
+    tweets = tweet.order_by("-date").all()
+
+    return JsonResponse([tweets.sterilize() for tweet in tweets], safe=False)
